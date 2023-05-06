63754ea187e930bd152eda5693967eaab915be5d
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-05-06 19:21:29 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,17 +2,30 @@ import React from 'react';
 import {HiMenuAlt3} from 'react-icons/hi';
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+    const handleNav = () => {
+        setNav(!nav);
+        if(!nav) {
+            document.body.style.overflow = 'hidden' 
+        } else {
+            document.body.style.overflow = 'scroll'
+        }
+    };
   return (
     <div className='absolute w-full flex justify-between p-4 items-center'>
         <h1 className='text-white font-bold text-2xl z-20'>Experiences</h1>
-        <HiMenuAlt3 size={25} className='text-white cursor-pointer' />
-        <div>
-            <ul>
-                <li>Home</li>
-                <li>Destinations</li>
-                <li>Reservations</li>
-                <li>Amenities</li>
-                <li>Rooms</li>
+        <HiMenuAlt3 onClick={handleNav} size={25} className='text-white cursor-pointer z-20' />
+        <div 
+            className={
+                nav
+                ? 'fixed ease-in duration-300 text-gray-300 left-0 top-0 w-full h-screen bg-black/90 py-7 flex-col z-10' 
+                :'absolute top-0 h-screen left-[-100%] ease-in duration-500 z-10'}>
+            <ul className='flex flex-col fixed w-full h-full items-center justify-center className="font-bold text-3xl p-8"'>
+                <li className="font-bold text-3xl p-8">Home</li>
+                <li className="font-bold text-3xl p-8">Destinations</li>
+                <li className="font-bold text-3xl p-8">Reservations</li>
+                <li className="font-bold text-3xl p-8">Amenities</li>
+                <li className="font-bold text-3xl p-8">Rooms</li>
             </ul>
         </div>
     </div>
