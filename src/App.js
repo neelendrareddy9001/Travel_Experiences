c596ea682d413bfd4b5253fea16e29087acfc642
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-05-06 15:30:30 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Hero from './components/Hero';
+import Navba from './components/Navbar';
 
 const App = () => {
   return (
     <div>
+      <Navbar/>
       <Hero/>
     </div>
   )
