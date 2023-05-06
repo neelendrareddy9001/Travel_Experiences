201baca09c6cefb2ed2a71383adaa4beebb6737d
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-05-06 20:07:51 +0200

    Add files via upload

---
+++ src/components/Offer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Offer = () => {
+  return (
+    <div className='max-w-[900px] m-auto py-12 px-4 flex flex-wrap justify-between'>
+        <p className='text-lg font-bold text-gray-700'>Booking Incentves</p>
+        <p className='text-lg font-bold text-gray-700'>Competitive</p>
+        <p className='text-lg font-bold text-gray-700'>Book with Flexibility</p>
+        <p className='text-lg font-bold text-gray-700'>Trip Insurance</p>
+    </div>
+  )
+}
+
+export default Offer
