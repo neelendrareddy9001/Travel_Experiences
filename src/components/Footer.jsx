ad420b1d55b964494fc744cf94a7ae17e81d21fc
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-05-09 22:11:35 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,45 +4,45 @@ import {TiSocialPinterest} from 'react-icons/ti';
 
 const Footer = () => {
   return (
-    <div>
-        <div>
+    <div className='w-full mt-24 bg-gray-900 text-gray-300 py-2 px-2'>
+        <div className='max-w-[1400px] mx-auto grid grid-cols-2 md:grid-cols-6 border-b-2 border-gray-600 py-8 px-4'>
             <div>
-                <h6>Solutions</h6>
+                <h6 className='font-bold uppercase pt-2'>Solutions</h6>
                 <ul>
-                    <li>Travel</li>
-                    <li>Booking</li>
-                    <li>Flight</li>
-                    <li>Cruises</li>
-                    <li>Ground</li>
+                    <li className='py-1'>Travel</li>
+                    <li className='py-1'>Booking</li>
+                    <li className='py-1'>Flight</li>
+                    <li className='py-1'>Cruises</li>
+                    <li className='py-1'>Ground</li>
                 </ul>
             </div>
             <div>
-                <h6>Support</h6>
+                <h6 className='font-bold uppercase pt-2'>Support</h6>
                 <ul>
-                    <li>Pricing</li>
-                    <li>Documentation</li>
-                    <li>Tours</li>
-                    <li>Refunds</li>
+                    <li className='py-1'>Pricing</li>
+                    <li className='py-1'>Documentation</li>
+                    <li className='py-1'>Tours</li>
+                    <li className='py-1'>Refunds</li>
                 </ul>
             </div>
             <div>
-                <h6>Company</h6>
+                <h6 className='font-bold uppercase pt-2'>Company</h6>
                 <ul>
-                    <li>About</li>
-                    <li>Blog</li>
-                    <li>Jobs</li>
-                    <li>Press</li>
-                    <li>Partners</li>
+                    <li className='py-1'>About</li>
+                    <li className='py-1'>Blog</li>
+                    <li className='py-1'>Jobs</li>
+                    <li className='py-1'>Press</li>
+                    <li className='py-1'>Partners</li>
                 </ul>
             </div>
             <div>
-                <h6>Legal</h6>
+                <h6 className='font-bold uppercase pt-2'>Legal</h6>
                 <ul>
-                    <li>Claims</li>
-                    <li>Privacy</li>
-                    <li>Terms</li>
-                    <li>Policies</li>
-                    <li>Conditions</li>
+                    <li className='py-1'>Claims</li>
+                    <li className='py-1'>Privacy</li>
+                    <li className='py-1'>Terms</li>
+                    <li className='py-1'>Policies</li>
+                    <li className='py-1'>Conditions</li>
                 </ul>
             </div>
             <div>
