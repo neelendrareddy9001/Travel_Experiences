88c710fa887f327cacce237a914b03780187d40b
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-05-09 22:36:23 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -45,22 +45,22 @@ const Footer = () => {
                     <li className='py-1'>Conditions</li>
                 </ul>
             </div>
-            <div>
-                <p>Subscribe to our newsletters</p>
-                <p>The Latest deals, articles and resources sent to your inbox weekly.</p>
-                <form>
-                    <input type='email' placeholder='Enter email..'/>
+            <div  className='col-span-2 py-8 md:pt-2'>
+                <p className='font-bold '>Subscribe to our newsletters</p>
+                <p className='py-4'>The Latest deals, articles and resources sent to your inbox weekly.</p>
+                <form className='flex flex-col sm:flex-row'>
+                    <input className='w-full p-2 mr-4 rounded-md mb-4' type='email' placeholder='Enter email..'/>
                     <button className='p-2 mb-4 rounded-md'>Subscribe</button>
                 </form>
             </div>
         </div>
-        <div>
-            <p>2022 Experiences, LLC, All rights reserved</p>
-            <div>
+        <div className='flex flex-col max-w-[1400px] px-2 py-4 mx-auto justify-between sm:flex-row text-center text-gray-500'>
+            <p className='py-4'>2022 Experiences, LLC, All rights reserved</p>
+            <div className='flex justify-between sm:w-[300px] pt-4 text-2xl'>
                 <FaFacebook />
                 <FaInstagram/>
                 <FaTwitter/>
-                <TiSocialPinterest/>
+                <TiSocialPinterest  size={30}/>
             </div>
         </div>
     </div>
