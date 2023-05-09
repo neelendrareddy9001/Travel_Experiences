356b447882f1a78b65edc598e5b0b317efd8a080
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-05-09 21:42:59 +0200

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {FaFacebook, FaInstagram, FiTwitter} from 'react-icons/fa';
+import {TiSocialPinterest} from 'react-icons/ti';
+
+const Footer = () => {
+  return (
+    <div>Footer</div>
+  )
+}
+
+export default Footer
